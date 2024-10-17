0e089c9a5917ab2887842a56a2bd64f3e3a30211
Anton Zhiganov <Antohajigan@gmail.com>
2024-10-18 00:04:42 +0200

    Task_2 is ready

---
+++ Lesson_1/Task_2/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float num_1;
+    float num_2;
+    char action;
+    float powResult;
+    cout << "Enter the first number" << endl;
+    cin >> num_1;
+    cout << "Select action, write below +, -, /, *, or pow (write 'P')" << endl;
+    cin >> action;
+    cout << "Enter the second number" << endl;
+    cin >> num_2;
+    switch (action){
+    case '+':
+        cout << "Result: " << num_1 + num_2 << endl;
+        break;
+    case '-':
+        cout << "Result: " << num_1 - num_2 << endl;
+        break;
+    case '/':
+        if (num_2 != 0) {
+            cout << "Result: " << num_1 / num_2 << endl;
+        }
+        else {
+            cout << "Error: Division by zero!" << endl;
+        }
+        break;
+    case '*':
+        cout << "Result: " << num_1 * num_2 << endl;
+        break;
+    case 'P':
+        powResult = pow(num_1, num_2);
+        cout << "Result: " << powResult << endl;
+        break;
+    default:
+        cout << "Invalid operation!" << endl;
+    }
+
+    return 0;
+}
