9ec712fbf2a6718796bbea284ec44cb55ff8f7b8
Anton Zhiganov <Antohajigan@gmail.com>
2024-11-02 17:20:57 +0100

    Removed functions from task 5 of lecture 1

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -5,37 +5,28 @@
 
 using namespace std;
 
-void drawTriangle1(int row) {
-    for (int element = 1; element <= row; element++) {
-        for (int numEl = 1; numEl <= element; numEl++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-}
-
-void drawTriangle2(int row) {
-    for (int element = 1; element <= row; element++) {
-        for (int numEl = 1; numEl <= row - element; numEl ++) {
-            cout << " ";
-        }
-        for (int numEl = 1; numEl <= element; numEl++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-}
-
 int main() {
     int row = 5;
 
     while (true) {
-
-        drawTriangle1(row);
+        for (int element = 1; element <= row; element++) {
+            for (int numEl = 1; numEl <= element; numEl++) {
+                cout << "*";
+            }
+            cout << endl;
+        }
         this_thread::sleep_for(chrono::seconds(1));
         system("cls");
 
-        drawTriangle2(row);
+        for (int element = 1; element <= row; element++) {
+            for (int numEl = 1; numEl <= row - element; numEl++) {
+                cout << " ";
+            }
+            for (int numEl = 1; numEl <= element; numEl++) {
+                cout << "*";
+            }
+            cout << endl;
+        }
         this_thread::sleep_for(chrono::seconds(1));
         system("cls");
     }
