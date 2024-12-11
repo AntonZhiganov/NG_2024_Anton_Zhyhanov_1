e78404bf977f2a99af3b2b941fc37c4cb7ad0479
Anton Zhiganov <Antohajigan@gmail.com>
2024-12-11 17:04:56 +0100

    corrected errors in Lesson_2 Task_5

--- Lesson_2/Task_5/main.cpp
+++ Lesson_2/Task_5/main.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 #include <string>
-#include <regex>
 
 using namespace std;
 
 int countWords(const string& str) {
-    regex wordRegex(R"(\b[a-zA-Z0-9]+\b)");
-    auto wordsBegin = sregex_iterator(str.begin(), str.end(), wordRegex);
-    auto wordsEnd = sregex_iterator();
-    return distance(wordsBegin, wordsEnd);
+    int wordCount = 0;
+    bool inWord = false;
+
+    for (char simbol : str) {
+        if (isalnum(simbol)) {
+            if (!inWord) {
+                inWord = true;
+                wordCount++;
+            }
+        } else {
+            inWord = false;
+        }
+    }
+
+    return wordCount;
 }
 
 int main() {
-
     string text;
-    cout << "Enter line: " << endl;
+    cout << "Enter line: ";
     getline(cin, text);
-    cout << "You line: " << text << endl;
+    cout << "Your line: " << text << endl;
     cout << "Number of words: " << countWords(text) << endl;
     return 0;
 }
